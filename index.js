368d58dc29c2ccae5cd5923d79a1b71adfc573d6
perfection1st <elijah@0v3rKill-PC.localdomain>
2022-09-01 17:11:56 -0500

    Features 1-3

--- index.js
+++ index.js
@@ -1,5 +1,71 @@
 (function() {
   'use strict';
-
-  // YOUR CODE HERE
+  let stopButton = document.getElementById('stopButton');
+  let slowButton = document.getElementById('slowButton');
+  let goButton = document.getElementById('goButton');
+  
+  let stop = function () {
+  let redLight = document.getElementById('stopLight');  
+  redLight.classList.toggle('stop');
+  if(redLight.classList.contains('stop')) {
+    console.log('Red bulb on');
+   } else {
+    console.log('Red bulb off');
+   }
+  }
+  
+  stopButton.addEventListener('click', stop);
+  
+  stopButton.addEventListener('mouseenter', () => {
+    console.log('Enter stop button');
+  });
+  
+  stopButton.addEventListener('mouseleave', () => {
+    console.log('Left stop button');
+  });
+  
+  
+  
+  let slow = function () {
+    let orangeLight = document.getElementById('slowLight');
+    orangeLight.classList.toggle('slow');
+    if(orangeLight.classList.contains('slow')) {
+      console.log('Orange bulb on');
+    } else {
+      console.log('Orange bulb off');
+    }
+  }
+  
+  slowButton.addEventListener('click',slow);
+  
+  slowButton.addEventListener('mouseenter', () => {
+    console.log('Enter slow button');
+  });
+  
+  slowButton.addEventListener('mouseleave', () => {
+    console.log('Left slow button');
+  });
+  
+    // YOUR CODE HERE
+  
+    let go = function () {
+      let greenLight = document.getElementById('goLight');
+      greenLight.classList.toggle('go');
+      if(greenLight.classList.contains('go')) {
+        console.log('Green bulb on');
+      } else {
+        console.log('Green bulb off');
+      }
+  
+    }
+  
+    goButton.addEventListener('click',go)
+  
+    goButton.addEventListener('mouseenter', () => {
+      console.log('Enter go button');
+    });
+  
+    goButton.addEventListener('mouseleave', () => {
+      console.log('Left go button');
+    });
 })();
